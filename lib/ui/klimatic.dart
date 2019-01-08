3b0f6225d61201ed5e132cb5d3df027436ba3195
Chris <christian.macias@cceo.com.mx>
2019-01-07 19:01:56 -0600

    setup

--- lib/ui/klimatic.dart
+++ lib/ui/klimatic.dart
@@ -12,11 +12,41 @@ class _KlimaticState extends State<Klimatic> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Klimatic"),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
         actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: null)
 
+        ],
+      ),
+
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset("images/umbrella.png",
+            width: 500,
+             height: 1200,
+             fit: BoxFit.fill,
+             ),
+            ),
+
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0, 11, 21, 0),
+            child: new Text('Spokane',
+              style: cityStyle(),),
+          )
         ],
       ),
     );
   }
+}
+
+TextStyle cityStyle(){
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 23,
+    fontStyle: FontStyle.italic
+  );
 }
